9c3e744a87638fe1f641e1193fa92226c6d96462
techflashYT <officialTechflashYT@gmail.com>
2024-10-30 10:09:59 -0700

    internal-loader: 4.6.0 support

--- internal-loader/init.sh
+++ internal-loader/init.sh
@@ -117,6 +117,7 @@ case $(cat /proc/version) in
     "Linux version 3.15.10"*) ver=installer ;;
     "Linux version 4.4.302-cip80-wii-ios"*) ver=v4_4_302;;
     "Linux version 4.5"*) ver=v4_5_0;;
+    "Linux version 4.6"*) ver=v4_6_0;;
     "Linux version 4.20"*) ver=v4_20_0;;
     "Linux version 6.6"*) ver=v6_6_0;;
     *) error "Unknown kernel version, Techflash messed up!"; support ;;
