aaa48e43855b3f831c65a5c95a15925c7e4ac103
techflashYT <officialTechflashYT@gmail.com>
2025-02-11 12:18:11 -0700

    ldr-img,boot_menu: bump versions

--- loader-img-full/init.sh
+++ loader-img-full/init.sh
@@ -16,7 +16,7 @@ fi
 exec < /dev/console > /dev/console 2> /dev/console
 
 reset
-echo "Wii Linux loader.img init v0.4.1"
+echo "Wii Linux loader.img init v0.5.0"
 
 cat /proc/sys/kernel/printk > /._printk_restore
 printf "1\t4\t1\t7" > /proc/sys/kernel/printk
@@ -120,7 +120,6 @@ while true; do
 	success "MIGRATION COMPLETE"
 	sleep 5
 done
-
 
 while true; do
 	/bin/boot_menu "$bootmenu_args"
