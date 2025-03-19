314b922dbeae937e5b4a05f0fb8c5f8d4a8bd294
techflashYT <officialTechflashYT@gmail.com>
2025-03-18 20:13:00 -0700

    internal-loader: add 4.9 support

--- internal-loader/init.sh
+++ internal-loader/init.sh
@@ -123,6 +123,8 @@ case $(cat /proc/version) in
     "Linux version 4.5.0-wii-ppcdroid"*) ver=v4_5_0a;;
     "Linux version 4.5"*) ver=v4_5_0;;
     "Linux version 4.6"*) ver=v4_6_0;;
+    "Linux version 4.9.337"*) ver=v4_9_337;;
+    "Linux version 4.9"*) ver=v4_9_0;;
     "Linux version 4.20"*) ver=v4_20_0;;
     "Linux version 6.6"*) ver=v6_6_0;;
     *) error "Unknown kernel version, Techflash messed up!"; support ;;
