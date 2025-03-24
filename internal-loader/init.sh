92dd2442ccb586305dd3eaf6e088aea9a459b4cd
techflashYT <officialTechflashYT@gmail.com>
2025-03-23 17:59:13 -0700

    int-ldr: fix version detection issue w/ 4.19

--- internal-loader/init.sh
+++ internal-loader/init.sh
@@ -108,8 +108,8 @@ case $(cat /proc/version) in
     "Linux version 4.9"*) ver=v4_9_0;;
     "Linux version 4.14.336"*) ver=v4_14336;;
     "Linux version 4.14"*) ver=v4_14_0;;
-    "Linux version 4.19"*) ver=v4_19_0;;
     "Linux version 4.19.325"*) ver=v4_19325;;
+    "Linux version 4.19"*) ver=v4_19_0;;
     "Linux version 4.20"*) ver=v4_20_0;;
     "Linux version 5.4"*) ver=v5_4_0;;
     "Linux version 6.6"*) ver=v6_6_0;;
