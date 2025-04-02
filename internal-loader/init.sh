6a00ff2a274109d5668e1808460186603b1c0f0d
techflashYT <officialTechflashYT@gmail.com>
2025-04-01 18:41:48 -0700

    int-ldr: bump version

--- internal-loader/init.sh
+++ internal-loader/init.sh
@@ -25,7 +25,7 @@ fi
 exec > /dev/console 2> /dev/console
 
 echo -en "\033[6;1H\033[J"
-echo "Wii Linux Initrd Loader v0.2.3"
+echo "Wii Linux Initrd Loader v0.2.4"
 
 echo "initrd starting" > /dev/kmsg
 . /logging.sh
