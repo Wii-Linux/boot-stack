b441ea4dd8012c5ec35370ddc417b8ec4594fc7d
techflashYT <officialTechflashYT@gmail.com>
2025-06-30 16:23:00 -0700

    add cleanup handler for PC

--- loader-img-full/term.h
+++ loader-img-full/term.h
@@ -3,3 +3,7 @@ extern int TERM_Height;
 
 extern void TERM_Init(struct termios *oldt);
 extern void TERM_DoResize(int dummy);
+
+#if !defined(PROD_BUILD) && !defined(DEBUG_WII)
+extern void cleanupAndExit(int dummy);
+#endif
